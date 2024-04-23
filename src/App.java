ef2fffd9202868759fc9a35dfd38f3997895e86a
silicao3o <zergskybmw@gmail.com>
2024-04-23 10:54:31 +0900

    1차

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("첫 번째 숫자를 입력하세요: ");
+        int num1 = sc.nextInt();
+        System.out.println("두 번째 숫자를 입력하세요: ");
+        int num2 = sc.nextInt();
+
+
+
+    }
+}
