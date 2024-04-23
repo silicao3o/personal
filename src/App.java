0607069823041fc7e7051511921fff33e64dbde6
silicao3o <zergskybmw@gmail.com>
2024-04-23 12:12:33 +0900

    기호 저장

--- src/App.java
+++ src/App.java
@@ -5,11 +5,16 @@ public class App {
         Scanner sc = new Scanner(System.in);
 
         System.out.println("첫 번째 숫자를 입력하세요: ");
-        int num1 = sc.nextInt();
+        int num1 = sc.nextInt(); // 첫번째 숫자 입력
         System.out.println("두 번째 숫자를 입력하세요: ");
-        int num2 = sc.nextInt();
+        int num2 = sc.nextInt(); // 두번째 숫자 입력
 
-
+        System.out.println("사칙연산 기호를 입력하세요: ");
+        String sign = "+-*/";
+        char plus = sign.charAt(0);
+        char minus = sign.charAt(1);
+        char multi = sign.charAt(2);
+        char div = sign.charAt(3);
 
     }
 }
