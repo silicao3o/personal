bfa60dfe5304d203a2f560a352392ca474182123
silicao3o <zergskybmw@gmail.com>
2024-05-01 12:40:20 +0900

    접근제어자 재수정

--- src/calculator/App.java
+++ src/calculator/App.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
-    private static void main(String[] args) {
+    public static void main(String[] args) {
         CircleCalculator calc = new CircleCalculator();
         ArithmeticCalculator calc2 = new ArithmeticCalculator();
         Scanner sc = new Scanner(System.in);
